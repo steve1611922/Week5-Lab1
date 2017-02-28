fee424fb5af76c5e5ed8065bee70cc31fa460209
steve1611922 <1611922@rgu.ac.uk>
2017-02-28 14:05:49 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+/**
+ * Created by PhpStorm.
+ * User: 1611922
+ * Date: 28/02/2017
+ * Time: 14:00
+ */
+<! DOCTYPE html >
+<html>
+ <head>
+</head>
+<body>
+ <p>
+ <?php
+ echo "Hello World";
+ ?>
+</p>
+</body>
+</html>
