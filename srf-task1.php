8e3b1541a030e52f59b9e9c75c4a935fa46ca3d7
steve1611922 <1611922@rgu.ac.uk>
2017-03-02 22:05:33 +0000

    Another way to code hello world

--- srf-task1.php
+++ srf-task1.php
@@ -5,9 +5,13 @@
     <title>Week5-Lab-1</title>
 </head>
 <body>
+<a href="http://example.com/search.html ?query=cat & type=image">Blarg</a>
 <p>
 <?php
 // php code goes here
+$query = $_GET["query"];
+$type = $_GET["type"];
+echo "The query is " . $query . "and the type is " . $type;
 ?>
 </p>
 </body>
