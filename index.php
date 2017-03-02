926955c2485fa36c5dc8444ba0c927f0a7080e94
steve1611922 <1611922@rgu.ac.uk>
2017-03-02 21:58:13 +0000

    Another way to code hello world

--- index.php
+++ index.php
@@ -5,30 +5,11 @@
      <title>Week5-Lab-1</title>
 </head>
 <body>
-<form action="action_page.php" method="post" id="login">
-    <fieldset>
-        <legend>Enter your username and password.</legend>
-        <label for="uname">Username:</label>
-        <input type="text" name="uname" value="steve" title="firstname" autofocus required><br><br>
-        <label for="pws">Password:</label>
-        <input type="password" name="pws" value="mysecret" title="lastname" required><br>
-        <input type="submit" value="Submit"><br>
-    </fieldset>
-</form>
-<?php
-    echo "Hello ".$uname;
-    if (isset($_COOKIE["access_level"]))
-    {
-        echo "Your access level is " . $_COOKIE["access_level"];
-    }
-    else
-    {
-        echo "You are are not logged in";
-    }
-    //displayAccessLevelInfo($_COOKIE["access_level"]);
-    echo "Done.";
-?>
- <p>
+<p>Week 5 Tutorials</p>
+<a href="cs-act1.php">Sessions and Cookies - Activity 1</a>
+<a href="srf-task1.php">Self referencing forms - Task 1</a>
+
+<p>
     <?php
 
         /*echo "Hello World";
---
+++ srf-task1.php
@@ -0,0 +1,14 @@
+<! DOCTYPE html >
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Week5-Lab-1</title>
+</head>
+<body>
+<p>
+<?php
+// php code goes here
+?>
+</p>
+</body>
+</html>
