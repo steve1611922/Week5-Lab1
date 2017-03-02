c30e3f96b63f873105c91497246a94dae56cad0f
steve1611922 <1611922@rgu.ac.uk>
2017-03-02 22:17:39 +0000

    Another way to code hello world

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 </head>
 <body>
 <p>Week 5 Tutorials</p>
+<a href="intro.php">Intro to PHP  - Activity VII</a>
+<br>
 <a href="cs-act1.php">Sessions and Cookies - Activity 1</a>
 <br>
 <a href="srf-task1.php">Self referencing forms - Task 1</a>
---
+++ intro.php
@@ -0,0 +1,31 @@
+<! DOCTYPE html >
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Week5-Introduction to PHP- Activity VII</title>
+</head>
+<body>
+<a href="http://example.com/search.html ?query=cat & type=image">Blarg</a>
+<p>
+    <?php
+    // php code goes here
+    echo "<br>"; echo "W A N T E D";echo "<br>";
+    wanted ($winner,3,5,7);echo "<br>";
+    wanted ($players[8],1,3,5);echo "<br>";
+    wanted ($players[2],2,1,12);echo "<br>";
+    // The wanted poster function.
+    function wanted ($name, $specsOwned, $mugsOwned, $rollsOwned)
+    {
+        echo "<br>Wanted:".strtoupper($name);
+        echo "<br>Known to be in possession of the following items:";
+        echo "<br>Specs(".$specsOwned.")";
+        echo "<br>Mugs(".$mugsOwned.")";
+        echo "<br>Sausage Rolls(".$rollsOwned.")";
+        //echo "<br>Award for capture £".($specsOwned*$mugsOwned*$rollsOwned)/2;
+        $reward = ($specsOwned*$mugsOwned*$rollsOwned)/2;
+        printf("<br>Award for capture £%.2f ", $reward);
+    }
+    ?>
+</p>
+</body>
+</html>
