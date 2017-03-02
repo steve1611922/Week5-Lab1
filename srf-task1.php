a539539401eaa0a487bf2c115558126a7ea60190
steve1611922 <1611922@rgu.ac.uk>
2017-03-02 22:08:37 +0000

    Another way to code hello world

--- srf-task1.php
+++ srf-task1.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Week5-Lab-1</title>
+    <title>Week5-Lab-Self Referencing Forms</title>
 </head>
 <body>
 <a href="http://example.com/search.html ?query=cat & type=image">Blarg</a>
