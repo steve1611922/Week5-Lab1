52704c892b4e14bd123cff2224215fe127785c7e
steve1611922 <1611922@rgu.ac.uk>
2017-03-04 14:20:23 +0000

    Another way to code hello world

--- srf-task1.php
+++ srf-task1.php
@@ -5,15 +5,9 @@
     <title>Week5-Lab-Self Referencing Forms- Task1</title>
 </head>
 <body>
-<a href="http://example.com/search.html?query=cat&type=image">example.com</a>
 <p>
 <?php
     // php code goes here
-    //print( '<a href="http://example.com/search.html?query=cat&type=image">example.com</a>' );
-    $query = $_GET["query"];
-    $type = $_GET["type"];
-    echo "The query is " . $query . "and the type is " . $type;
-    echo "<br>";
     print( '<a href="helloSelector.php">Go to Hello Selector</a>' );
 ?>
 </p>
