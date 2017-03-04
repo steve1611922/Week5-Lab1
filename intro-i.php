5511d14c7df983f454dd8097ad6d68d000e8faaf
steve1611922 <1611922@rgu.ac.uk>
2017-03-04 11:25:45 +0000

    Another way to code hello world

--- intro-i.php
+++ intro-i.php
@@ -13,7 +13,7 @@
     if ($myage > 16) { echo $myname.". You may buy specs.<br>";}
     if($myage > 18) {echo $myname.". You may also buy mugs.<br>";}
     if ($myage > 21) {echo $myname.". You may also buy sausage rolls.<br>";}
-
+    else {echo "You are too young tpo enjoy life...";}
 ?>
 </p>
 </body>
