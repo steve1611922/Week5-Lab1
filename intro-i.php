ab504229e0fa88ad0e214d90bf24cefa7f1f26ce
steve1611922 <1611922@rgu.ac.uk>
2017-03-04 11:41:10 +0000

    Another way to code hello world

--- intro-i.php
+++ intro-i.php
@@ -22,10 +22,12 @@
     //$myname="Steve";
     $myname = $_POST["myname"];
     $myage = $_POST["myage"];
-    if ($myage > 16) { echo ucfirst($myname).", ou may buy specs.<br>";}
-    if($myage > 18) {echo ucfirst($myname).", you may also buy mugs.<br>";}
-    if ($myage > 21) {echo ucfirst($myname).", you may also buy sausage rolls.<br>";}
-    else {echo "You are too young too enjoy life...";}
+    if ($myage <= 16){ echo "You are to young to enjoy life...";}
+    else {
+        if ($myage > 16) { echo ucfirst($myname).", ou may buy specs.<br>";}
+        if ($myage > 18) {echo ucfirst($myname).", you may also buy mugs.<br>";}
+        if ($myage > 21) {echo ucfirst($myname).", you may also buy sausage rolls.<br>";}
+    }
 ?>
 </p>
 </body>
