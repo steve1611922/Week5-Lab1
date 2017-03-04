1458e9bcaacf4bd39d4923b3c3552640181064ca
steve1611922 <1611922@rgu.ac.uk>
2017-03-04 15:02:26 +0000

    Another way to code hello world

--- avatar.php
+++ avatar.php
@@ -12,6 +12,7 @@
     $lname = $_POST["lname"];
     $gender = $_POST["gender"];
     $power = $_POST["power"];
+    echo "T H I S  I S  Y O U R  A V A T A R <br>";
     echo "Your name is ".$fname." ".$lname.".<br>";
     echo "You are ".$gender.".<br>";
     echo "Your superpower is ".$power.".<br>";
