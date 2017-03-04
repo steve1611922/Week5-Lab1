4d2a521c5cdd605154aa9470df2ad1eff704c99f
steve1611922 <1611922@rgu.ac.uk>
2017-03-04 15:07:10 +0000

    Another way to code hello world

--- srf-task2.php
+++ srf-task2.php
@@ -10,8 +10,8 @@
         <legend>Super hero, enter your details...</legend>
         <label for="fname">First Name:</label><input type="text" name="fname" value=""><br>
         <label for="lname">Last Name:</label><input type="text" name="lname" value=""><br>
-        <label for="gender">Male:</label><input type="radio" name="gender" value="male"><br>
-        <label for="gender">Female:</label><input type="radio" name="gender" value="female"><br>
+        <label for="gender">Male</label><input type="radio" name="gender" value="male"><br>
+        <label for="gender">Female</label><input type="radio" name="gender" value="female"><br>
         <label for="power">Superpower:</label><input type="text" name="power" value=""><br>
         <input type="submit" value="Register" />
     </fieldset>
