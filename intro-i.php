5251b9540b392b43b202eae3c9bb06a24a078787
steve1611922 <1611922@rgu.ac.uk>
2017-03-04 11:35:45 +0000

    Another way to code hello world

--- intro-i.php
+++ intro-i.php
@@ -5,15 +5,27 @@
     <title>Title</title>
 </head>
 <body>
+<form action="" method="post">
+    <fieldset>
+        <legend>Tell me your name and age...</legend>
+        <label for="myname:">Username:</label>
+        <input type="text" name="myname" value="steve" title="myname"><br><br>
+        <label for="myage">Myage:</label>
+        <input type="number" name="myage" value="16" title="myage"><br>
+        <input type="submit" value="Submit"><br>
+    </fieldset>
+</form>
 <p>
     <?php
     // php code goes here
-    $myage=16;
-    $myname="Steve";
+    //$myage=19;
+    //$myname="Steve";
+    $myname = $_POST["myname"];
+    $myage = $_POST["myage"];
     if ($myage > 16) { echo $myname.". You may buy specs.<br>";}
     if($myage > 18) {echo $myname.". You may also buy mugs.<br>";}
     if ($myage > 21) {echo $myname.". You may also buy sausage rolls.<br>";}
-    else {echo "You are too young tpo enjoy life...";}
+    else {echo "You are too young too enjoy life...";}
 ?>
 </p>
 </body>
