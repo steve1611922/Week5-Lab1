2b609e3f3a470256e765bb47a28d8ee320390870
steve1611922 <1611922@rgu.ac.uk>
2017-03-04 17:52:47 +0000

    Another way to code hello world

--- mikecrabb-srf.php
+++ mikecrabb-srf.php
@@ -4,33 +4,32 @@
     <title>selfreference</title> 
 </head> 
 <body>
- <?php
-if ($_SERVER["REQUEST_METHOD"] === "GET") {
-?>
-    <!--execute if requested using HTTP GET Method -->
-    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
-        <fieldset>
-            <legend>Enter your details</legend>
-            <label>Forename</label> <input type="text" name="forename" title="forename"> 
-            <label>Surname</label> <input type="text" name="surname" title="surname"> 
-            <input type="submit" value="Submit">
-        </fieldset>
-    </form>
 <?php
+if ($_SERVER["REQUEST_METHOD"] ==="GET") {
+    //get code
+    $forename = $_GET["forename"];
+    $surname = $_GET["surname"];
+    print ("Hello {$forename} {$surname}");
+
 }
-elseif ($_SERVER["REQUEST_METHOD"] === "POST") {
-     
-    // execute if requested using HTTP POST Method 
+elseif ($_SERVER["REQUEST_METHOD"]=== "POST"){
+    //post code
     $forename = $_POST["forename"];
-     
     $surname = $_POST["surname"];
-     
-    print("<h1>Hello {$forename} {$surname}</h1>");
-} 
-else {
-    // this is impossible 
-    echo "impossible";
+    print ("Hello {$forename} {$surname}");
 }
-?> 
+else {
+    // can never happen
+    echo "an error has occurred";
+}
+?>
+<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+    <fieldset>
+        <legend>Enter your details</legend>
+        <label>Forename</label> <input type="text" name="forename" title="forename"> 
+        <label>Surname</label> <input type="text" name="surname" title="surname"> 
+        <input type="submit" value="Submit">
+    </fieldset>
+</form>
 </body> 
 </html>
