e3baa08409c33e314f0aebcda7eea15ce393af01
steve1611922 <1611922@rgu.ac.uk>
2017-03-04 14:18:46 +0000

    Another way to code hello world

--- srf-task1.php
+++ srf-task1.php
@@ -5,11 +5,11 @@
     <title>Week5-Lab-Self Referencing Forms- Task1</title>
 </head>
 <body>
-<!--a href="http://example.com/search.html ?query=cat & type=image">http://example.com/search.html</a-->
+<a href="http://example.com/search.html?query=cat&type=image">example.com</a>
 <p>
 <?php
     // php code goes here
-    print( '<a href="http://example.com/search.html?query=cat&]type=image">example.com</a>' );
+    //print( '<a href="http://example.com/search.html?query=cat&type=image">example.com</a>' );
     $query = $_GET["query"];
     $type = $_GET["type"];
     echo "The query is " . $query . "and the type is " . $type;
