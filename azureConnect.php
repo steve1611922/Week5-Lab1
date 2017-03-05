1efb522ba606ac245c314540cb690cfd6e45adcd
steve1611922 <1611922@rgu.ac.uk>
2017-03-05 14:55:58 +0000

    Another way to code hello world

--- azureConnect.php
+++ azureConnect.php
@@ -2,7 +2,7 @@
 // PHP Data Objects(PDO) Sample Code:
 try {
 $conn = new PDO("sqlsrv:server = tcp:rgu-labs.database.windows.net,1433; 
-    Database = labs-db", "swcadmin", "{your_password_here}");
+    Database = labs-db", "swcadmin", "Crabby123");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -11,7 +11,7 @@ die(print_r($e));
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "swcadmin@rgu-labs", "pwd" => "{your_password_here}",
+$connectionInfo = array("UID" => "swcadmin@rgu-labs", "pwd" => "Crabby123",
     "Database" => "labs-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:rgu-labs.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
