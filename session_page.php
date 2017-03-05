98be51c151e74e9bbea6e3a1039aa470cdd45507
steve1611922 <1611922@rgu.ac.uk>
2017-03-05 12:43:42 +0000

    Another way to code hello world

--- session_page.php
+++ session_page.php
@@ -7,9 +7,10 @@
 <body>
 <p>
     <?php
-    //Start the session
+    //Set session values
     $_SESSION["session_level"] = "standarduser";
     $_SESSION["customer"] = $_POST["uname"];
+    echo $_SESSION["session_level"].$_SESSION["customer"];
     echo "<br>";
     echo "You can check your session data by visiting this link<br>";
     print( '<a href="session-homepage.php">Check session data</a>' );
