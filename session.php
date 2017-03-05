30f72703489b727ef7afd84051f8f7bc21af212d
steve1611922 <1611922@rgu.ac.uk>
2017-03-05 18:04:09 +0000

    Another way to code hello world

--- session.php
+++ session.php
@@ -1,17 +1,20 @@
 <?php
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysql_connect("localhost", "root", "");
-// Selecting Database
-$db = mysql_select_db("company", $connection);
+//$connection = mysql_connect("localhost", "root", "");
+$connectionInfo = array("UID" => "swcadmin@rgu-labs", "pwd" => "Crabby123",
+    "Database" => "labs-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:rgu-labs.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 session_start();// Starting Session
 // Storing Session
 $user_check=$_SESSION['login_user'];
 // SQL Query To Fetch Complete Information Of User
-$ses_sql=mysql_query("select username from login where username='$user_check'", $connection);
-$row = mysql_fetch_assoc($ses_sql);
-$login_session =$row['username'];
+$sql = "select username from login where username='$user_check'";
+$query = sqlsrv_query ($conn, $sql);
+$row=sqlsrv_fetch_array($query);
+$login_session = $row['username'];
 if(!isset($login_session)){
-    mysql_close($connection); // Closing Connection
-    header('Location: index.php'); // Redirecting To Home Page
+    sqlsrv_close($conn); // Closing Connection
+    header('Location: formget.php'); // Redirecting To Home Page
 }
 ?>
