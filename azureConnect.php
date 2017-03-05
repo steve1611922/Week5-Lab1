bbf2d95e932028edd05b15cf0ecb99975c52f12e
steve1611922 <1611922@rgu.ac.uk>
2017-03-05 17:15:16 +0000

    Another way to code hello world

--- azureConnect.php
+++ azureConnect.php
@@ -44,7 +44,7 @@ echo "<br>";
   } else {
       print("Table login created.\n");
   }
-    /*$sql = "INSERT INTO login (id,username,password) VALUES (2,'donald', 'duck')";
+    $sql = "INSERT INTO login (id,username,password) VALUES (3,'pinky', 'perky')";
     $res = sqlsrv_query ($conn, $sql);
     if (!$res) {
         print("Insert failed with error:\n");
@@ -57,7 +57,7 @@ echo "<br>";
     } else {
         print("Table row created.\n");
     }
-    */
+
     $sql = "select * from login where password='$password' AND username='$username'";
     $query = sqlsrv_query ($conn, $sql);
     echo "Rows:".$query;
